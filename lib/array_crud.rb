650ec442862d521c2edf3170a3ace953e745d59b
Brooke Jones <bjones782@gmail.com>
2019-05-12 19:34:55 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -17,7 +17,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  colors = ["red", "blue", "green", "pink"]
+  colors.pop
 end
 
 def remove_element_from_start_of_array(array)
